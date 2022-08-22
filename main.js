d5ea5da5d0fca8a2d508e57b7c229a4f204ac0c0
NUHR-24 <56066254+Pooly-21@users.noreply.github.com>
2022-08-22 23:38:15 +0200

    almost done
    need some assistance with the size and one by one changing

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 function clickdiv(element, color){
-    document.getElementById("color1").style.background="red";
+    document.getElementById("color1").style.background = "red";
+    document.getElementById("color2").style.background = "orange";
+    document.getElementById("color3").style.background = "yellow";
+    document.getElementById("color4").style.background = "green";
+    document.getElementById("color5").style.background = "blue";
+    document.getElementById("color6").style.background = "purple";
+    document.getElementById("color7").style.background = "violet";
     element.style.color = color;
 }
